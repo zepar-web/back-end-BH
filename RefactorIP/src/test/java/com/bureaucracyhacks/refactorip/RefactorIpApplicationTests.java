fdac180a4807ec15b50f627a240bbc438cfea8af
Robertino103 <69898258+Robertino103@users.noreply.github.com>
2023-04-06 23:38:06 +0300

    refactorized first week progress

---
+++ RefactorIP/src/test/java/com/bureaucracyhacks/refactorip/RefactorIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bureaucracyhacks.refactorip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactorIpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
