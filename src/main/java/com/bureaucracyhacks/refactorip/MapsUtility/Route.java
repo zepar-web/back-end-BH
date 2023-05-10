282e87b24045950d332a39680c1834c6330effbb
zepar-web <radu.robert1101@gmail.com>
2023-05-10 21:29:57 +0300

    fixed geoLocation api

--- src/main/java/com/bureaucracyhacks/refactorip/MapsUtility/Route.java
+++ src/main/java/com/bureaucracyhacks/refactorip/MapsUtility/Route.java
@@ -1,5 +1,7 @@
 package com.bureaucracyhacks.refactorip.MapsUtility;
 
+import com.google.maps.model.Bounds;
+
 import java.util.ArrayList;
 
 public class Route
@@ -7,7 +9,7 @@ public class Route
     public Bounds bounds;
     public String copyrights;
     public ArrayList<Leg> legs;
-    public OverviewPolyline overview_polyline;
+    //public OverviewPolyline overview_polyline;
     public String summary;
     public ArrayList<String> warnings;
     public ArrayList<Object> waypoint_order;
